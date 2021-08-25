f8b1045ff07938ae9c006c538e9f66cdb721ad23
paumoreno26 <55001929+paumoreno26@users.noreply.github.com>
2021-08-25 08:45:10 -0500

    Add files via upload

---
+++ src/main/test/java/com/pruebanuvuv/PruebaNuvuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebanuvuv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebanuvuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
